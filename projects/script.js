08c5e7a2d157bcf69e8d63f04cbdec8bbaa57cfa
Fozle Rabbi <infiniterabbiex@gmail.com>
2025-07-23 12:17:07 -0700

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -50,14 +50,10 @@ function showProjects(projects) {
       <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
-        <h3>${project.name}</h3>
+        <h3>${project.desc}</h3>
         </div>
         <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
+          <p>${project.name}</p>
         </div>
       </div>
     </div>
